7cbaa352b9e6abd13bacae6305f995cde2f56ca7
Suhail Idrees <gusmanbhai@hotmail.com>
2017-03-21 03:25:06 -0400

    Hard-code poster images on entry screen

---
+++ components/showThumb.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import {
+  Dimensions,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+import pics from '../shows.json'
+
+
+const WINDOW_WIDTH = Dimensions.get('window').width;
+const WINDOW_HEIGHT = Dimensions.get('window').height;
+
+export default class showThumb extends React.Component {
+  render () {
+    return (
+      <ScrollView style={styles.outside}>
+        <View style={styles.container}>
+          <Image 
+          source={require('../images/gaycation.jpg')}
+          style={styles.poster}>
+          </Image>
+          <Image 
+          source={require('../images/huangsworld.jpg')}
+          style={styles.poster}>
+          </Image>
+          <Image 
+          source={require('../images/kingoftheroad.jpg')}
+          style={styles.poster}>
+          </Image>
+          <Image 
+          source={require('../images/lastchancehigh.jpg')}
+          style={styles.poster}>
+          </Image>
+          <Image 
+          source={require('../images/loveindustries.jpg')}
+          style={styles.poster}>
+          </Image>
+          <Image 
+          source={require('../images/pilotweek.jpg')}
+          style={styles.poster}>
+          </Image>
+          <Image 
+          source={require('../images/statesofundress.jpg')}
+          style={styles.poster}>
+          </Image>
+          <Image 
+          source={require('../images/vicehbo.jpg')}
+          style={styles.poster}>
+          </Image>
+          <Image 
+          source={require('../images/woman.jpg')}
+          style={styles.poster}>
+          </Image>
+          <Image 
+          source={require('../images/worldofsports.jpg')}
+          style={styles.poster}>
+          </Image>
+        </View>
+       </ScrollView>
+    )
+  }
+}
+
+let styles = StyleSheet.create({
+  outside: {
+    backgroundColor: '#141414',
+  },
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    marginTop: 50,
+    alignContent: 'row',
+    margin: 8,
+  },
+  poster: {
+    height: WINDOW_HEIGHT / 4.5,
+    width: WINDOW_WIDTH / 3.5,
+    display: 'flex',
+    margin: 5,
+  }
+});
--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 import Expo from 'expo';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Show from './components/showThumb'
 
 class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Frontend matster!</Text>
-      </View>
+      <Show style={{backgroundColor: 'black'}}/>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     backgroundColor: 'black',
+//   },
+// });
 
 Expo.registerRootComponent(App);
