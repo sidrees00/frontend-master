44581b68914eeac4213d4efe60530554ed33d32c
Suhail Idrees <gusmanbhai@hotmail.com>
2017-03-21 04:43:28 -0400

    Add header and "Featured Shows" heading

--- components/showThumb.js
+++ components/showThumb.js
@@ -18,6 +18,17 @@ export default class showThumb extends React.Component {
   render () {
     return (
       <ScrollView style={styles.outside}>
+        <View>
+          <TouchableHighlight 
+            onPress={()=> (alert('clicked'))}
+            style={styles.poster}>
+              <Image 
+                source={require('../images/vicehboCopy.jpg')}
+                style={styles.header}>
+              </Image>
+          </TouchableHighlight>
+        </View>
+          <Text style={styles.text}> Featured Shows </Text>
         <View style={styles.container}>
           <TouchableHighlight 
             onPress={()=> (alert('clicked'))}
@@ -79,14 +90,6 @@ export default class showThumb extends React.Component {
             onPress={()=> (alert('clicked'))}
             style={styles.poster}>
               <Image 
-                source={require('../images/vicehbo.jpg')}
-                style={styles.poster}>
-              </Image>
-          </TouchableHighlight>
-          <TouchableHighlight 
-            onPress={()=> (alert('clicked'))}
-            style={styles.poster}>
-              <Image 
                 source={require('../images/woman.jpg')}
                 style={styles.poster}>
               </Image>
@@ -108,20 +111,38 @@ export default class showThumb extends React.Component {
 let styles = StyleSheet.create({
   outside: {
     backgroundColor: '#141414',
+    display: 'flex',
   },
   container: {
     display: 'flex',
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'space-between',
-    marginTop: 50,
+    marginTop: 5,
     alignContent: 'row',
     margin: 8,
+    marginRight: 15,
   },
   poster: {
     height: WINDOW_HEIGHT / 4.5,
     width: WINDOW_WIDTH / 3.5,
     display: 'flex',
     margin: 5,
+    marginTop: 5,
+  },
+  header: {
+    height: WINDOW_HEIGHT / 4.2,
+    width: WINDOW_WIDTH / 1.1,
+    display: 'flex',
+    margin: 11,
+    marginTop: 10,
+    marginBottom: 200,
+  },
+  text: {
+    color: 'white',
+    fontSize: 15,
+    marginLeft: 15,
+    marginTop: 25,
+    marginBottom: -5,
   }
 });
